ad45826d4a809f5227db2d085ca6d052133f4441
Hardicine <hardicine@mail.ru>
2024-11-28 21:33:15 +0300

    Правка 28.11 (2 задания, пойду отдыхать)

--- Laba_5_2/Task2.c
+++ Laba_5_2/Task2.c
@@ -10,23 +10,23 @@ void main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	int size_A, size_B, i = 0;
+	int size_A, size_B, i = 0; 
 
 	puts("Введите кол-во элементов массива A и B:");
-	while (scanf("%d %d", &size_A, &size_B) != 2 || size_A < 1 || size_A > MAX_SIZE || size_B < 1 || size_B > MAX_SIZE)
+	while (scanf("%d %d", &size_A, &size_B) != 2 || size_A < 1 || size_A > MAX_SIZE || size_B < 1 || size_B > MAX_SIZE) // Вводим размерности двух массивов и проверяем их
 	{
 		while (getchar() != '\n');
 		system("cls");
 		puts("Вы ввели что-то не так...\nВведите другое кол-во элементов в массивах:");
 	}
 
-	double arr_A[MAX_SIZE], arr_B[MAX_SIZE];
+	double arr_A[MAX_SIZE], arr_B[MAX_SIZE]; // Объявляем эти два массива
 
 	puts("\nВведите элементы массива A:");
 	for (i; i < size_A; i++)
 	{
 		printf("arr_A[%2d] = ", i + 1);
-		while (scanf("%lf", &arr_A[i]) != 1)
+		while (scanf("%lf", &arr_A[i]) != 1) // Заполняем массив A и проверяем его
 		{
 			while (getchar() != '\n');
 			puts("Вы ввели что-то не так...\nВведите элемент массива снова:");
@@ -34,36 +34,36 @@ void main()
 		}
 	}
 
-	int min = 0;
+	int min = 0; // Объявляем вспомогательную переменную и присваиваем ей номер самого первого элемента массива
 
 	puts("\nВведите элементы массива B:");
 	for (i = 0; i < size_B; i++)
 	{
 		printf("arr_B[%2d] = ", i + 1);
-		while (scanf("%lf", &arr_B[i]) != 1)
+		while (scanf("%lf", &arr_B[i]) != 1) // Заполняем массив B и проверяем его
 		{
 			while (getchar() != '\n');
 			puts("Вы ввели что-то не так...\nВведите элемент массива снова:");
 			printf("arr_B[%2d] = ", i + 1);
 		}
-		if (arr_B[min] > arr_B[i])
+		if (arr_B[min] > arr_B[i]) // Параллельно ищем самый ЛЕВЫЙ МИНИМАЛЬНЫЙ элемент
 		{
 			min = i;
 		}
 	}
 
-	double arr_C[MAX_SIZE];
+	double arr_C[MAX_SIZE]; // Объявляем массив C
 	i = 0;
 
 	puts("\nМассив C будет иметь вид:");
 
-	for (int j = 3; j <= size_A; j += 4, i++)
+	for (int j = 3; j <= size_A; j += 4, i++) // Выбираем из массива A значения элементов, у которых номера кратны 4, и присваиваем их массиву C
 	{
 		arr_C[i] = arr_A[j];
 		printf("arr_C[%2d] = %.1lf\n", i + 1, arr_C[i]);
 	}
 
-	for (int j = 0; j <= min; j++, i++)
+	for (int j = 0; j < min; j++, i++) // Добавляем к массиву C значения элементов, которые стоят слева от самого левого минимального значения из массива B
 	{
 		arr_C[i] = arr_B[j];
 		printf("arr_C[%2d] = %.1lf\n", i + 1, arr_C[i]);
